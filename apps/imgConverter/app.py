7c47360c9647de3bc620b3a13438084606411b7a
natyosu3 <nachi334825@gmail.com>
2023-06-25 02:19:41 +0900

    update

--- apps/imgConverter/app.py
+++ apps/imgConverter/app.py
@@ -1,33 +1,48 @@
-from flask import Flask, render_template, request, flash, redirect, url_for
-# ファイル名をチェックする関数
+from flask import Flask, render_template, redirect, request, url_for
 from werkzeug.utils import secure_filename
-# 画像のダウンロード
-from flask import send_from_directory
-
 import os
-
+import subprocess
+import threading
 
 app = Flask(__name__)
-
+app.config['convert_finished'] = False  # フラグをアプリケーションの設定に格納します
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/convert", methods=["GET", "POST"])
+@app.route("/convert/", methods=["GET", "POST"])
 def convert():
     if request.method == 'POST':
         file = request.files['file']
-        file.save(os.getcwd() + '/static/images/' + file.filename)
-        return redirect(url_for('uploaded_file', filename=file.filename))
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.root_path, 'static/images', filename))
+
+        th = threading.Thread(target=convert_pdf, args=(filename, ))
+        th.start()
+
+        return redirect(url_for('uploaded_file', filename=filename))
 
     return render_template("convert_page.html")
 
-
 @app.route('/uploaded_file/<string:filename>')
 def uploaded_file(filename):
+    # アプリケーションの設定からフラグの値をチェックしてリダイレクトを行う
+    if app.config['convert_finished']:
+        return redirect(url_for('finished', filename=filename))
+
     return render_template('uploaded_file.html', filename=filename)
 
+@app.route('/finished/<string:filename>')
+def finished(filename):
+    return render_template('finish.html')
+
+def convert_pdf(filename):
+    result = subprocess.Popen(f"ffmpeg -i {os.path.join(os.getcwd(), 'apps', 'imgConverter', 'static', 'images', filename)} {os.path.join(os.getcwd(), 'apps', 'imgConverter', 'static', 'images', filename + '.avi')}", stdout=subprocess.DEVNULL)
+    result.wait()
+
+    # アプリケーションの設定のフラグを設定する
+    app.config['convert_finished'] = True
 
 if __name__ == "__main__":
     app.run(debug=True)
