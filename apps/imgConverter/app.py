3afd850b0137850f2abd9db6adfce18680302261
natyosu3 <nachi334825@gmail.com>
2023-06-27 00:10:00 +0900

    update

--- apps/imgConverter/app.py
+++ apps/imgConverter/app.py
@@ -1,48 +1,55 @@
 from flask import Flask, render_template, redirect, request, url_for
 from werkzeug.utils import secure_filename
 import os
-import subprocess
-import threading
+import magic
+import time
 
 app = Flask(__name__)
-app.config['convert_finished'] = False  # フラグをアプリケーションの設定に格納します
+
+app.config['MAX_CONTENT_LENGTH'] = 100 * 1000 * 1000
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/convert/", methods=["GET", "POST"])
-def convert():
-    if request.method == 'POST':
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == "POST":
         file = request.files['file']
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.root_path, 'static/images', filename))
+        file_data = file.stream.read()
 
-        th = threading.Thread(target=convert_pdf, args=(filename, ))
-        th.start()
+        file_type = magic.from_buffer(file_data, mime=True)
+        file.stream.seek(0)  # ファイルの読み取り位置を先頭に戻す
 
-        return redirect(url_for('uploaded_file', filename=filename))
+        if file_type == "image/png":
+            file.save(os.path.join(app.root_path, 'static/images', secure_filename(file.filename)))
+            return redirect(url_for('wait_page', filename=file.filename))
+        else:
+            return "DAMN"
+        
+    if request.method == "GET":
+        return render_template("convert_page.html")
 
-    return render_template("convert_page.html")
 
-@app.route('/uploaded_file/<string:filename>')
-def uploaded_file(filename):
-    # アプリケーションの設定からフラグの値をチェックしてリダイレクトを行う
-    if app.config['convert_finished']:
-        return redirect(url_for('finished', filename=filename))
+@app.route('/wait/<filename>')
+def wait_page(filename):
+    # 変換処理の実行（非同期など）
+    time.sleep(3)
 
-    return render_template('uploaded_file.html', filename=filename)
+    # 変換終了ページにリダイレクト
+    return redirect(url_for('finish_page', filename=filename))
 
-@app.route('/finished/<string:filename>')
-def finished(filename):
-    return render_template('finish.html')
 
-def convert_pdf(filename):
-    result = subprocess.Popen(f"ffmpeg -i {os.path.join(os.getcwd(), 'apps', 'imgConverter', 'static', 'images', filename)} {os.path.join(os.getcwd(), 'apps', 'imgConverter', 'static', 'images', filename + '.avi')}", stdout=subprocess.DEVNULL)
-    result.wait()
+@app.route('/finish/<filename>')
+def finish_page(filename):
+    return render_template('finish.html', filename=filename)
 
-    # アプリケーションの設定のフラグを設定する
-    app.config['convert_finished'] = True
+
+@app.errorhandler(404)
+def page_not_found(error):
+  return render_template("404.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
